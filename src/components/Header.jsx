21d02c541fb32f96df5abfd2ffaea7359ce8e5f3
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-10 12:33:13 -0400

    finish Header comp. add toggling feature to hamburger btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,47 @@ import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import { useLocation } from "react-router-dom";
 import { Button } from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 export function Header() {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      disablePageScroll();
+      setOpenNavigation(false);
+    } else {
+      enablePageScroll();
+      setOpenNavigation(true);
+    }
+  };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    setOpenNavigation(false);
+    disablePageScroll();
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave Logo" />
         </a>
-        <nav className="hidden fixed top-[5rem] right-0 left-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] right-0 left-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -24,11 +55,13 @@ export function Header() {
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover-text-n-1 xl:px-12`}
+                onClick={handleClick}
               >
                 {item.title}
               </a>
             ))}
           </div>
+          <HamburgerMenu />
         </nav>
         <a
           href="#signup"
@@ -37,6 +70,13 @@ export function Header() {
           New Account
         </a>
         <Button className="hidden lg:flex">Sign in</Button>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
       </div>
     </div>
   );
