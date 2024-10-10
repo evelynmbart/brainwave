97bb8abab2d4cd054e5a25db423d5c873ab0d4e9
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-10 13:56:43 -0400

    implementing and styling hero robot image. install react-just-parallax

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { Section } from "./Section";
-import { curve } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
 import { Button } from "./Button";
 
 export function Hero() {
@@ -14,7 +14,7 @@ export function Hero() {
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilites of AI Chatting with BrainwaveCurve
+            Explore the Possibilites of AI Chatting with{" "}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -31,6 +31,31 @@ export function Hero() {
             with Brainwave, the open AI chat app.
           </p>
           <Button white>Get Started</Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-22">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem] " />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  className="w-full scale-[1.7] translate-y-[8%] md:sclare-[1] md:-translate-y-[10%] lg:-translate-y-[30%]"
+                  width={1024}
+                  height={490}
+                  alt="ai"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1140}
+              height={1800}
+              alt="hero"
+            />
+          </div>
         </div>
       </div>
     </Section>
